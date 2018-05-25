e19f0e6c886bb72268814bdfdc2df98a470d50e3
Edsel Caprice <caprice@mail.usf.edu>
2018-05-25 01:05:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $(document).on('keypress',function(key) {
+  $(document).on('keydown',function(key) {
     if (key.which == 71) {
       alert('g was pressed');
     }
